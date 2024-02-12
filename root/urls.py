600678c6544f5c86b18a2293ff37e1d1964210fd
your username here <your email here>
2024-02-12 20:27:18 +0500

    last  answers

--- root/urls.py
+++ root/urls.py
@@ -1,13 +1,8 @@
-from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
-
-from root.settings import STATIC_URL, MEDIA_URL, STATIC_ROOT, MEDIA_ROOT, DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.urls'))
 ]
-if DEBUG:
-    urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
-    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
